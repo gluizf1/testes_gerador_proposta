a9872f8cd91a45abfef14ef62baf4a5d837fea30
gluizf1 <150209247+gluizf1@users.noreply.github.com>
2025-10-03 15:22:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,9 +65,9 @@ with st.sidebar:
 
     # ---------- Botão de Configurações no final ----------
     st.markdown("", unsafe_allow_html=True)  # força espaço até o final
-    col1, col2, col3 = st.columns([20,1,1])
+    col1, col2, col3 = st.columns([1,1,1])
     with col2:
-        st.button(" ⚙️ ", on_click=abrir_configuracoes, help="Configurações")
+        st.button("⚙️", on_click=abrir_configuracoes, help="Configurações")
 
 # ---------- Página de Proposta ----------
 if st.session_state["pagina"] == "proposta":
